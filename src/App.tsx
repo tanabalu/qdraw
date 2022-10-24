a295d2a6626acd4ca45828de6f5a0295eafc99d7
满昌 <cx362562@antgroup.com>
2022-10-24 14:41:42 +0800

    fix: 修复router路径匹配问题

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import Home from '@/pages/Home';
 import './App.css';
 
 function App() {
-  return <Home />;
-  // return (
-  //   <BrowserRouter>
-  //     <Routes>
-  //       <Route path="/" element={<Home />} />
-  //     </Routes>
-  //   </BrowserRouter>
-  // );
+  return (
+    // 这里的 basename 匹配 vite.config.ts 里的 baseUrl 属性，否则路由会无法匹配
+    <BrowserRouter basename='qdraw'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
